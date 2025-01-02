26afb40109578c495052d4c25ec8f9d21cd7b02f
Michal Stvan <your.email@example.com>
2025-01-02 21:44:20 +0000

    test

--- recipe.sh
+++ recipe.sh
@@ -1,9 +1,15 @@
-mkdir android-kernel && mkdir bin && cd android-kernel
+git config --global user.name "Michal Stvan"
+git config --global user.email "dummy@example.org"
+
+mkdir ~/bin
+export PATH="$HOME/bin:$PATH"
+
+mkdir android-kernel && cd android-kernel
 
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 
 export REPO_COLOR_DISPLAY=1
 
-~/bin/repo init -u https://android.googlesource.com/kernel/manifest -b common-android14-6.1-lts
-~/bin/repo sync
+repo init -u https://android.googlesource.com/kernel/manifest -b common-android14-6.1-lts
+repo sync
